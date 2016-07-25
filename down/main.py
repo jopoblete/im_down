c7e1d23eb93b2daeb0ec1fedf551570f51796b34
Carolyn Hanson Lee <carolynlee9828@gmail.com>
2016-07-25 12:13:06 -0700

    Added blog files

--- down/main.py
+++ down/main.py
@@ -1,25 +1,95 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 import webapp2
+import jinja2
+import os
+from google.appengine.ext import ndb
+from google.appengine.api import users
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir))
+
+
+
+
+class Post(ndb.Model):
+    text = ndb.TextProperty()
+    title = ndb.StringProperty()
+    date = ndb.DateTimeProperty(auto_now_add=True)
+
+    def url(self):
+        return '/post?key='+ self.key.urlsafe() #you need to use self, not post.key.blah
+
+
+
+class Comment(ndb.Model):
+    text = ndb.TextProperty()
+    name = ndb.StringProperty()
+    date = ndb.DateTimeProperty(auto_now_add=True)
+    post_key = ndb.KeyProperty(kind=Post)
+
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        squirtle = Post.query().fetch()
+        template_values = {'charmander':squirtle} #fetch all the posts
+        template = jinja_environment.get_template('home.html')
+        self.response.write(template.render(template_values))
+
+    def post(self):
+        # Step 1: Get info from the Request
+        title = self.request.get('title')
+        text = self.request.get('text')
+        # Step 2: Logic -- interact with the database
+        post = Post(title=title, text=text)
+        post.put()
+
+
+        # Step 3: Render a response
+        self.redirect('/')
+
+
+
+
+class PostHandler(webapp2.RequestHandler):
+    def get(self):
+        # Step 1: Get info from the Request
+        urlsafe_key = self.request.get('key')
+
+        # Step 2: Logic -- interact with the database
+        key = ndb.Key(urlsafe=urlsafe_key)
+        post = key.get()
+
+        comments = Comment.query(Comment.post_key == post.key).fetch()
+
+
+        # Step 3: Render a response
+        template_values = {'post':post, 'comments':comments} #fetch all the posts
+        template = jinja_environment.get_template('post.html')
+        self.response.write(template.render(template_values))
+
+    def post(self):
+        # Step 1: Get info from the Request
+        text = self.request.get('comment') #the comment string
+        post_key_urlsafe = self.request.get('key') #string thats coming out of the request
+
+        # Step 2: Logic -- interact with the database
+        post_key = ndb.Key(urlsafe=post_key_urlsafe) #go from a string to a key
+        post = post_key.get() #turns into the entity ( the post )
+
+        comment = Comment(text=text, name= 'Loser', post_key=post.key)
+        comment.put()
+
+
+
+
+        # Step 3: Render a response
+        self.redirect(post.url())
+
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/post', PostHandler)
+
 ], debug=True)
