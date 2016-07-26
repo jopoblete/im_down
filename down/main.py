8f71e57dbf3b1738ebbf63662d336f0101ea1424
Demo User <demouser@MacBook-Air-10.local>
2016-07-26 14:53:13 -0700

    Idk

--- down/main.py
+++ down/main.py
@@ -10,31 +10,6 @@ import time
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir))
 
-class Pacific_tzinfo(datetime.tzinfo):
-    """Implementation of the Pacific timezone."""
-    def utcoffset(self, dt):
-        return datetime.timedelta(hours=-8) + self.dst(dt)
-
-    def _FirstSunday(self, dt):
-        """First Sunday on or after dt."""
-        return dt + datetime.timedelta(days=(6-dt.weekday()))
-
-    def dst(self, dt):
-        # 2 am on the second Sunday in March
-        dst_start = self._FirstSunday(datetime.datetime(dt.year, 3, 8, 2))
-        # 1 am on the first Sunday in November
-        dst_end = self._FirstSunday(datetime.datetime(dt.year, 11, 1, 1))
-
-        if dst_start <= dt.replace(tzinfo=None) < dst_end:
-            return datetime.timedelta(hours=1)
-        else:
-            return datetime.timedelta(hours=0)
-    def tzname(self, dt):
-        if self.dst(dt) == datetime.timedelta(hours=0):
-            return "PST"
-        else:
-            return "PDT"
-
 class User(ndb.Model):
     name = ndb.TextProperty()
     password = ndb.TextProperty()
@@ -74,15 +49,9 @@ class MainHandler(webapp2.RequestHandler):
         # Step 1: Get info from the Request
         text = self.request.get('text')
         # Step 2: Logic -- interact with the database
-
-
-
         post = Post(name = 'yungmarmar', text=text)
 
         post.put()
-
-
-
         # Step 3: Render a response
         self.redirect('/home')
 
@@ -95,7 +64,7 @@ class PostHandler(webapp2.RequestHandler):
         key = ndb.Key(urlsafe=urlsafe_key)
         post = key.get()
 
-        comments = Comment.query(Comment.post_key == post.key).fetch()
+        comments = Comment.query(Comment.post_key == post.key).order(-Post.date).fetch()
 
 
         # Step 3: Render a response
